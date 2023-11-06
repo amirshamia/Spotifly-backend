7073dddc6e1a4f828c3484c6f5452487bf65d9e4
amirshamia <amir0988@gmail.com>
2023-11-06 20:15:18 +0200

    changed prod url

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,4 @@
 export default {
-	dbURL: process.env.MONGO_URL ||'mongodb+srv://amir:<amirsh123>@cluster0.dsf6xvr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
-	dbName: process.env.DB_NAME || 'station',
+	dbURL: 'mongodb+srv://amir:amirsh123@cluster0.dsf6xvr.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
+	dbName:  'spotifly',
 }
